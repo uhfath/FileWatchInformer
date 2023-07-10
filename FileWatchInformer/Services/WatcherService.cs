ea5e578ce5cb0b8cbbc84dde392520a301d48bbd
Kasbolat Kumakhov <uhfath@gmail.com>
2023-07-10 14:58:34 +0300

    Добавлен метод поиска новых файлов с учётом всех критериев

--- FileWatchInformer/Services/WatcherService.cs
+++ FileWatchInformer/Services/WatcherService.cs
@@ -2,36 +2,123 @@
 using FileWatchInformer.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FileWatchInformer.Services
 {
     internal class WatcherService : BackgroundService
     {
-        private readonly IServiceScopeFactory _serviceScopeFactory;
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		private readonly IOptionsMonitor<WatcherConfig> _watcherConfig;
+		private readonly IOptionsMonitor<UsersConfig> _usersConfig;
+		private readonly IHostApplicationLifetime _applicationLifetime;
+		private readonly IServiceScopeFactory _serviceScopeFactory;
+		private readonly ILogger<WatcherService> _logger;
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                await using (var scope = _serviceScopeFactory.CreateAsyncScope())
+                try
                 {
-                    var emailSender = scope.ServiceProvider.GetRequiredService<EmailSenderService>();
+                    foreach (var user in _usersConfig.CurrentValue.Users)
+                    {
+						var folder = Path.Combine(_watcherConfig.CurrentValue.Folder, user.Folder);
+
+                        var excludeWildcard = string.IsNullOrWhiteSpace(user.ExcludeWildcard)
+                            ? _watcherConfig.CurrentValue.DefaultExcludeWildcard
+                            : user.ExcludeWildcard;
+
+                        var excludedFiles = new HashSet<string>();
+                        if (!string.IsNullOrWhiteSpace(excludeWildcard))
+                        {
+                            excludedFiles = Directory.GetFiles(folder, excludeWildcard, SearchOption.AllDirectories)
+                                .ToHashSet();
+                        }
+
+                        var includeWildcard = string.IsNullOrWhiteSpace(user.IncludeWildcard)
+                            ? "*.*"
+                            : user.IncludeWildcard;
+
+                        var filesQuery = Directory.EnumerateFiles(folder, includeWildcard, SearchOption.AllDirectories)
+                            .Where(f => !excludedFiles.Contains(f))
+                            .AsEnumerable()
+                        ;
+
+						var excludePattern = string.IsNullOrWhiteSpace(user.ExcludePattern)
+							? _watcherConfig.CurrentValue.DefaultExcludePattern
+							: user.ExcludePattern;
+
+						if (!string.IsNullOrWhiteSpace(excludePattern))
+                        {
+							var regex = new Regex(excludePattern, RegexOptions.Compiled);
+                            filesQuery = filesQuery
+                                .Where(f => !regex.IsMatch(Path.GetFileName(f)))
+                            ;
+						}
+
+						var includePattern = string.IsNullOrWhiteSpace(user.IncludePattern)
+							? _watcherConfig.CurrentValue.DefaultIncludePattern
+							: user.IncludePattern;
+
+						if (!string.IsNullOrWhiteSpace(includePattern))
+                        {
+							var regex = new Regex(includePattern, RegexOptions.Compiled);
+                            filesQuery = filesQuery
+                                .Where(f => regex.IsMatch(Path.GetFileName(f)))
+                            ;
+						}
+
+                        var files = filesQuery
+                            .ToArray()
+                        ;
+
+						if (files.Length > 0)
+                        {
+                            await using (var scope = _serviceScopeFactory.CreateAsyncScope())
+                            {
+                                var emailSender = scope.ServiceProvider.GetRequiredService<EmailSenderService>();
+
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Ошибка обработки");
                 }
 
-                await Task.Delay(5000);
+                if (_watcherConfig.CurrentValue.Interval == null)
+                {
+                    _applicationLifetime.StopApplication();
+				}
+                else
+                {
+                    await Task.Delay(_watcherConfig.CurrentValue.Interval.Value);
+                }
             }
         }
 
         public WatcherService(
-            IServiceScopeFactory serviceScopeFactory)
+            IHostApplicationLifetime applicationLifetime,
+            IServiceScopeFactory serviceScopeFactory,
+			IOptionsMonitor<WatcherConfig> watcherConfig,
+			IOptionsMonitor<UsersConfig> usersConfig,
+            ILogger<WatcherService> logger)
         {
-            _serviceScopeFactory = serviceScopeFactory;
-        }
+			this._applicationLifetime = applicationLifetime;
+			this._serviceScopeFactory = serviceScopeFactory;
+			this._watcherConfig = watcherConfig;
+			this._usersConfig = usersConfig;
+			this._logger = logger;
+		}
 
         public static IServiceCollection Configure(HostBuilderContext context, IServiceCollection services)
         {
